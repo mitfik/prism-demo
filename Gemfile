9e8edab53f2199a51cba5f1ae50d119fd14b6739
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2013-12-03 23:10:51 +0100

    Add DataMapper adapter with initialization

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 ruby "2.0.0"
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
--- prism.rb
+++ prism.rb
@@ -1,10 +1,24 @@
 require 'sinatra/base'
 require 'sinatra/config_file'
+require 'data_mapper'
 
 class Prism < Sinatra::Base
   register Sinatra::ConfigFile
 
   config_file File.join(ENV['CONFIG_FILE'] || "config/database.yml")
+
+  def self.initialize_database
+    begin
+      DataMapper.setup(:default, settings.database_uri)
+      DataMapper.finalize.auto_upgrade!
+    rescue => msg
+      raise "Problem with establish connection to database."
+    end
+  end
+
+  configure do
+    initialize_database
+  end
 
   get '/' do
     "Welcome in PRISM demo"
